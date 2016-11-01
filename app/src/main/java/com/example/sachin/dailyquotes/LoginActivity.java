7cb59d803551510bdfe08b44da8be76f668f9bdd
sachin <sn1628@nyu.edu>
2016-11-01 19:34:25 +0530

    initial project setup

---
+++ app/src/main/java/com/example/sachin/dailyquotes/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.sachin.dailyquotes;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sachin on 10/31/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+
+}
