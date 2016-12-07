2924b1e46e595e5e89f17c342d5367a539b1ef75
sachin <sn1628@nyu.edu>
2016-12-06 23:41:24 -0500

    first commit

--- app/src/main/java/com/example/sachin/dailyquotes/LoginActivity.java
+++ app/src/main/java/com/example/sachin/dailyquotes/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.sachin.dailyquotes;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by Sachin on 10/31/16.
@@ -8,5 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    @Bind(R.id.editText4) EditText  editUserName;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main_screen);
+
+    }
 }
