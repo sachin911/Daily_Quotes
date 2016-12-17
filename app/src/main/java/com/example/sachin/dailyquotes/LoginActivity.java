c6e5b0dec79d1c4e15346ea677b57e947ed925f0
sachin <sn1628@nyu.edu>
2016-12-17 11:40:00 -0500

    commenting out some lines

--- app/src/main/java/com/example/sachin/dailyquotes/LoginActivity.java
+++ app/src/main/java/com/example/sachin/dailyquotes/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.sachin.dailyquotes;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 
 /**
  * Created by Sachin on 10/31/16.
@@ -11,7 +10,7 @@ import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Bind(R.id.editText4) EditText  editUserName;
+   // @Bind(R.id.editText4) EditText  editUserName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
